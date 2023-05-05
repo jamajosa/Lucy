70daf5f5a7d4f364dc8a5b08177f834627f6a1dc
Jordy Huijgens <jordy.huijgens@outlook.com>
2023-05-05 10:32:22 +0200

    ci

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    transform: {
-      "^.+\\.(js|jsx|ts|tsx)$": "babel-jest", "node_modules/expo-font/.+\\.(j|t)sx?$": "babel-jest"
-    },
-  };
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+    "node_modules/expo-font/.+\\.(j|t)sx?$": "babel-jest"
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!react-native)/"
+  ]
+};
